d2b56ee28ab170f5461199066505bfb2b9f41453
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2021-11-22 23:20:12 -0300

    teste CORS

--- src/main/java/com/example/todolist/controller/TaskController.java
+++ src/main/java/com/example/todolist/controller/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:9001")
 @RestController
 @RequestMapping("/task")
 public class TaskController {
